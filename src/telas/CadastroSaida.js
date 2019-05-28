4902efbf8d4807ab58c2145e1da906f79f1b0a5b
Marcelo Carvalho <marcelo.scientist@hotmail.com>
2019-05-27 22:32:34 -0300

    Atualizando pag cadastro de saida

--- src/telas/CadastroSaida.js
+++ src/telas/CadastroSaida.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import {
+  MDBInput, MDBCardBody, MDBCardTitle, MDBBtn, MDBContainer,
+  MDBRow, MDBCol, MDBFreeBird, MDBEdgeHeader
+} from "mdbreact";
+//Rodrigo
 
-import '../css/searchBar.css';
-import { MDBInput, MDBBtn, MDBDatePicker } from "mdbreact";
 
 
-//Marcelo
+
 export default class CadastroSaida extends Component {
   showSettings(event) {
     event.preventDefault();
@@ -16,171 +17,62 @@ export default class CadastroSaida extends Component {
 
   render() {
     return (
-      <div>
-        <div id="page-wrap">
-          <h1>Cadastro Saída</h1>
-          <div className='container'>
-            <div className="row mb-2">
-              <div className="col-md-6">
-                <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                  <div className="col p-4 d-flex flex-column position-static">
-
-
-                    <br />
-
-                    <form className="alinhandoEsquerda">
-                    <MDBInput label="Nome" background icon="user" />
-                    </form>
-
-                    <br />
-                    <div className="input-group mb-3">
-                      <select className="custom-select" id="inputGroupSelect02" defaultValue="0">
-                        <option value="0" disabled>Choose...</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                      </select>
-                      <div className="input-group-append">
-                        <label className="input-group-text" for="inputGroupSelect02">Options</label>
-                      </div>
-                    </div>
-
-
-                    <br />
-                    <div className="input-group mb-3">
-                      <select className="custom-select" id="inputGroupSelect02" defaultValue="0">
-                        <option value="0" disabled>Choose...</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                      </select>
-                      <div className="input-group-append">
-                        <label className="input-group-text" for="inputGroupSelect02">Options</label>
-                      </div>
-                    </div>
-
-
-                    <br />
-                    <div className="input-group mb-3">
-                      <select className="custom-select" id="inputGroupSelect02">
-                        <option value="0" disabled>Choose...</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                      </select>
-                      <div className="input-group-append">
-                        <label className="input-group-text" for="inputGroupSelect02">Options</label>
-                      </div>
-                    </div>
-                    <div className="form-check">
-                      <input className="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="..." />
-                      <label className="form-check-label" for="gridCheck1">
-                        <strong className="d-inline-block mb-2 text-success">Notificar responsável</strong>
-                      </label>
-                    </div>
-
-
-                    <br />
-                    <div className="input-group mb-3">
-                      <select className="custom-select" id="inputGroupSelect02" defaultValue="0">
-                        <option value="0" disabled>Choose...</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                      </select>
-                      <div className="input-group-append">
-                        <label className="input-group-text" for="inputGroupSelect02">Options</label>
-                      </div>
-                    </div>
-
-
-                    <br />
-                    <div className="input-group mb-3">
-                      <select className="custom-select" id="inputGroupSelect02" defaultValue="0">
-                        <option value="0" disabled>Choose...</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                      </select>
-                      <div className="input-group-append">
-                        <label className="input-group-text" for="inputGroupSelect02">Options</label>
-                      </div>
-                    </div>
-
-
-                    <div className="form-check">
-                      <input className="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="..." />
 
 
 
-                      <label className="form-check-label" for="gridCheck1">
-                        <strong className="d-inline-block mb-2 text-success">Notificar professor</strong>
-                      </label>
-                    </div>
+      <MDBContainer style={{ marginTop: 50, marginBottom: 50 }}>
+        {/* <MDBEdgeHeader color="white"></MDBEdgeHeader>
+        <MDBFreeBird> */}
+        <MDBRow>
+          <MDBCol size="5" md="10" lg="5" className="white z-depth-3 py-2 px-2">
+            <MDBCardBody>
+              <MDBCardTitle></MDBCardTitle>
+              <form className='alinhandoEsquerda'>
+                <MDBInput label="Nome Completo" background icon="user" group type="text" id='nome' />
+                <MDBInput label="E-mail" background icon="envelope" color="success" group type='email' id='email' />
+                <MDBInput label="Senha" background icon="key" group type='password' id='senha' />
+                <MDBInput label="Telefone / WhatsApp" background icon="phone" group type='tel' id='telefone' />
 
-                    
-                  </div>
-
+                <div>
+                  <select className="custom-select custom-select-lg mb-5" id="categoria" defaultValue="1">
+                    <option disabled value="1">Tipo de Usuário</option>
+                    <option value="2">Administrador</option>
+                    <option value="3">Guarda</option>
+                  </select>
                 </div>
-              </div>
+
+              </form>
+              <MDBBtn color="success" className="text-xs-left">Salvar</MDBBtn>
+            </MDBCardBody>
+          </MDBCol>
+          <MDBCol size="5" md="10" lg="5" className="white z-depth-3 py-2 px-2">
+            <MDBCardBody>
+              <MDBCardTitle></MDBCardTitle>
+              <form className='alinhandoEsquerda'>
 
 
 
+                <MDBInput label="E-mail" background icon="envelope" color="success" group type='email' id='email' />
+                <MDBInput label="Senha" background icon="key" group type='password' id='senha' />
+                <MDBInput label="Telefone / WhatsApp" background icon="phone" group type='tel' id='telefone' />
 
-
-
-              <div className="col-md-6">
-                <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                  <div className="col p-4 d-flex flex-column position-static">
-
-                        <div className='alinhandoEsquerda'>
-                          <MDBInput label="Nome" type="date" background icon="calendar" />
-                        </div>
-                        {/*<script type="text/javascript">
-                          $(function () {
-                            $('#datetimepicker1').datetimepicker();
-                          });
-                        </script>*/}
-
-                    <br />
-                    <div className="input-group mb-3">
-                      
-                    <select className="custom-select custom-select-lg mb-5" id="categoria" defaultValue="1">
-                      <option disabled value="1">Tipo de Usuário</option>
-                      <option value="2">Administrador</option>
-                      <option value="3">Guarda</option>
-                    </select>
-                    </div>
-
-
-                    <br />
-                    <div className="input-group mb-3">
-                      <select className="custom-select" id="inputGroupSelect02" defaultValue="0">
-                        <option value="0" disabled>Choose...</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                      </select>
-                      <div className="input-group-append">
-                        <label className="input-group-text" for="inputGroupSelect02">Options</label>
-                      </div>
-                    </div>
-
-
-                    <div className="form-check">
-                      <input className="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="..." />
-                      <label className="form-check-label" for="gridCheck1">
-                        <strong className="d-inline-block mb-2 text-success">Example</strong>
-                      </label>
-                    </div>
-                  </div>
-
+                <div>
+                  <select className="custom-select custom-select-lg mb-5" id="categoria" defaultValue="1">
+                    <option disabled value="1">Tipo de Usuário</option>
+                    <option value="2">Administrador</option>
+                    <option value="3">Guarda</option>
+                  </select>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+
+              </form>
+              <MDBBtn color="success" className="text-xs-left">Salvar</MDBBtn>
+            </MDBCardBody>
+          </MDBCol>
+          <MDBCol size="2"></MDBCol>
+
+        </MDBRow>
+
+      </MDBContainer >
     );
   }
 }
