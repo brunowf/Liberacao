b4614997e656695998e249ce3180108a789b832b
Bruno Weiler <brunoweiller@hotmail.com>
2019-05-28 21:47:13 -0300

    Fazendo pasta Professor e separando em componentes

--- src/telas/Professor/MainProfessor.js
+++ src/telas/Professor/MainProfessor.js
@@ -1,82 +1,30 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import axios from 'axios';
-import { DataTable } from "mdbreact";
-import { urlServidor } from '../../Variaveis.json'
+import { MDBCardBody } from "mdbreact";
+import CadastroProfessor from './CadastroProfessor.js';
+import { urlServidor } from '../../Variaveis.json';
+import ListaProfessores from "./ListaProfessores.js";
 
 
 
 
 export default class MainProfessor extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      listaProfessores:{
-        columns:[
-          {
-            label: 'ID',
-            field: 'id',
-            sort: 'asc',
-            width: 150
-          },
-          {
-            label: 'Nome',
-            field: 'nome',
-            sort: 'asc',
-            width: 150
-          },
-          {
-            label: 'E-mail',
-            field: 'email',
-            sort: 'asc',
-            width: 270
-          },
-          {
-            label: 'Senha',
-            field: 'senha',
-            sort: 'asc',
-            width: 200
-          },
-          {
-            label: 'Data de Cadastro',
-            field: 'create_time',
-            sort: 'asc',
-            width: 200
-          },
-        ],
-        rows:[
-          {
 
-          }
-        ]
-      }
-    }
-  }
-
-  componentDidMount() {
-    axios.get(urlServidor + '/users/professores')
-      .then(resposta => {
-        //se deu certo:
-        //this.setState({ listaProfessores: resposta.data })
-        let data = {...this.state.listaProfessores}
-        data.rows = resposta.data
-        console.log(data)
-        this.setState( { listaProfessores : data } )
-
-      })
-      .catch(resposta => {
-        //se deu errado:
-        alert('Deu errado!')
-        console.log(resposta)
-      })
-  }
   render() {
     return (
-            <DataTable
-            striped
-            bordered
-            hover
-            data={this.state.listaProfessores}
-            />
+      <div className='container containerBorda' style={{ marginTop: 60, marginBottom: 60 }}>
+        <div className='row'>
+          <div className='col-sm-1'></div>
+          <div className='col-sm-10 alinhandoEsquerda z-depth-3'>
+
+            <MDBCardBody>
+              <ListaProfessores></ListaProfessores>
+              <CadastroProfessor></CadastroProfessor>
+            </MDBCardBody>
+          </div>
+          <div className='col-sm-1'></div>
+        </div>
+      </div>
     );
   }
 
