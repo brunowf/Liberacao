23ea68f3cf8ad19008667af84b50d0c91f483e59
Bruno Weiler <brunoweiller@hotmail.com>
2019-05-29 22:37:35 -0300

    Adicionando submit para cadastroturma

--- src/telas/Turma/CadastroTurma.js
+++ src/telas/Turma/CadastroTurma.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
-import { MDBInput, MDBBtn, MDBCardBody, MDBCardTitle } from "mdbreact";
+import { MDBInput, MDBBtn, MDBCardTitle } from "mdbreact";
+import axios from 'axios';
+import { urlServidor } from '../../Variaveis.json'
 //Bruno
 export default class CadastroTurma extends Component {
-  showSettings(event) {
-    event.preventDefault();
+  // showSettings(event) {
+  //   event.preventDefault();
+  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      turmaId: '',
+      turmaNome: '',
+      turmaPeriodo: '',
+      turmaFase: '',
+      turmaAnoSemestre: '',
+    }
+  }
+
+  turmaPost() {
+    axios.post(urlServidor + '/turmas', {
+      id: null,
+      nome_curso: this.state.turmaNome,
+      periodo: this.state.turmaPeriodo,
+      fase: this.state.turmaFase,
+      ano_semestre: this.state.turmaAnoSemestre
+    }).then(resposta => {
+      //se deu certo:
+      alert('deu certo')
+    })
+      .catch(resposta => {
+        //se der errado
+        alert('Deu errado!')
+      })
   }
 
   render() {
     return (
       <form>
         <MDBCardTitle>Cadastro de Turma</MDBCardTitle>
-        <MDBInput label="Turma" background icon="door-closed" />
-        <MDBInput label="Periodo" background icon="calendar-alt" />
-        <MDBInput label="Duração" background icon="hourglass" />
+        <MDBInput label="Nome da Turma" name="turmaNome" background icon="door-closed" onChange={(event => this.setState({ turmaNome: event.target.value }))} />
+        <MDBInput label="Periodo" background icon="calendar-alt" onChange={(event => this.setState({ turmaPeriodo: event.target.value }))} />
+        <MDBInput label="Fase" background icon="hourglass" onChange={(event => this.setState({ turmaFase: event.target.value }))} />
+        <MDBInput label="Ano / Semestre" background icon="hourglass" onChange={(event => this.setState({ turmaAnoSemestre: event.target.value }))} />
         <div className='tyleBotao'>
-          <MDBBtn className="dusty-grass-gradient">Salvar</MDBBtn>
+          <MDBBtn className="dusty-grass-gradient" onClick={() => this.turmaPost()}>Salvar</MDBBtn>
         </div>
       </form>
     );
