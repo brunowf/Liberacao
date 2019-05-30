48dc9a0ebe8002e0f5f4764efeb4910f8ff1dfd8
Bruno Weiler <brunoweiller@hotmail.com>
2019-05-30 20:15:20 -0300

    adicionando select ao cadastro de turma

--- src/telas/Login.js
+++ src/telas/Login.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 //Daniel
 
--- src/telas/Turma/CadastroTurma.js
+++ src/telas/Turma/CadastroTurma.js
@@ -12,7 +12,7 @@ export default class CadastroTurma extends Component {
     this.state = {
       turmaId: '',
       turmaNome: '',
-      turmaPeriodo: '',
+      turmaPeriodo: 'MATUTINO',
       turmaFase: '',
       turmaAnoSemestre: '',
     }
@@ -40,9 +40,15 @@ export default class CadastroTurma extends Component {
       <form>
         <MDBCardTitle>Cadastro de Turma</MDBCardTitle>
         <MDBInput label="Nome da Turma" name="turmaNome" background icon="door-closed" onChange={(event => this.setState({ turmaNome: event.target.value }))} />
-        <MDBInput label="Periodo" background icon="calendar-alt" onChange={(event => this.setState({ turmaPeriodo: event.target.value }))} />
         <MDBInput label="Fase" background icon="hourglass" onChange={(event => this.setState({ turmaFase: event.target.value }))} />
         <MDBInput label="Ano / Semestre" background icon="hourglass" onChange={(event => this.setState({ turmaAnoSemestre: event.target.value }))} />
+        <select className="browser-default custom-select" defaultValue="MATUTINO" onChange={(event => this.setState({ turmaPeriodo: event.target.value }))} >
+          <option disabled>Período</option>
+          <option value="MATUTINO">Matutino</option>
+          <option value="VESPERTINO">Vespertino</option>
+          <option value="DIURNO">Diurno</option>
+          <option value="NOTURNO">Noturno</option>
+        </select>
         <div className='tyleBotao'>
           <MDBBtn className="dusty-grass-gradient" onClick={() => this.turmaPost()}>Salvar</MDBBtn>
         </div>
