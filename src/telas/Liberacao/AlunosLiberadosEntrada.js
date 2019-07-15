e923b7d2bf245a92b5ebff00e81961df2cf493a4
Bruno Weiler <brunoweiller@hotmail.com>
2019-07-15 20:57:15 -0300

    Arrumando colunas dos cards no alunosliberadosentrada

--- src/telas/Liberacao/AlunosLiberadosEntrada.js
+++ src/telas/Liberacao/AlunosLiberadosEntrada.js
@@ -1,5 +1,5 @@
 import React, { Component } from '../../../node_modules/react';
-import { MDBCardBody, MDBBtn, MDBContainer } from "mdbreact";
+import { MDBBtn, MDBContainer } from "mdbreact";
 import api from '../../services/api';
 import { urlServidor } from '../../Variaveis.json';
 //V1.0 Daniel, V1.1 Marcelo, V1.2 Rodrigo
@@ -13,8 +13,8 @@ export default class AlunosLiberadosEntrada extends Component {
   componentDidMount() {
     api.get(urlServidor + '/registroentradas')
       .then(resposta => {
-        this.setState({ listaEntrada: resposta.data })
-        console.log(this.state.listaEntrada)
+        this.setState({ listaEntrada: resposta.data})
+        console.log(resposta)
       })
       .catch(resposta => {
         //se deu errado:
@@ -40,7 +40,7 @@ export default class AlunosLiberadosEntrada extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      listaEntrada: []
+      listaEntrada: [],
     }
   }
 
@@ -49,19 +49,29 @@ export default class AlunosLiberadosEntrada extends Component {
       <MDBContainer style={{ marginTop: 60, marginBottom: 60 }}>
         <div className='row'>
           <div className='col-sm-1'></div>
-          <div className='col-sm-10 alinhandoEsquerda z-depth-3'>
-            <div className="text-center">
-              <div className="text-capitalize card-group" style={{ marginTop: 20, marginBottom: 5 }}>
-                  {this.state.listaEntrada.map(lista =>
-                  <div className="card" style={{ marginTop: 10, marginBottom: 5 }}>
-                    <div className="card-body text-center">
-                      <p className="card-text font-weight-bold" style={{fontSize:20}}>{lista.aluno.nome}</p>
-                      <MDBBtn outline color="danger" onClick={() => this.entradaDelete(lista.id)}>Deletar</MDBBtn>
+          <div className='col-sm-10 alinhandoEsquerda'>
+
+          
+
+            <div>
+              <div className='card-columns' style={{ marginTop: 20, marginBottom: 5 }}>
+                
+                {this.state.listaEntrada.map(lista =>
+                  <div className='text-capitalize' key={lista.id}>
+                    <div className="card" style={{ marginTop: 10, marginBottom: 5 }}>
+                      <div className="card-body text-center">
+                        <p className="card-text font-weight-bold" style={{ fontSize: 20 }}>{lista.aluno.nome}</p>
+                        <MDBBtn outline color="danger" onClick={() => this.entradaDelete(lista.id)}>Deletar</MDBBtn>
+                      </div>
                     </div>
                   </div>
                 )}
+                
               </div>
             </div>
+
+
+
           </div>
           <div className='col-sm-1'></div>
         </div>
@@ -69,72 +79,3 @@ export default class AlunosLiberadosEntrada extends Component {
     );
   }
 }
-
-
-// <div id="App">
-      //   <div id="page-wrap">
-      //     <h2>Alunos Liberados Entrada</h2>
-
-      //     <div className="card-group">
-
-            // <div className="card">
-            //   <div className="card-body text-center">
-            //     <img alt='Imagem do aluno' src="imagens/loiramaria.jpg" width="260" height="160"></img>
-            //     <p className="card-text">Proximo Nayala</p>
-            //   </div>
-            // </div>
-
-      //       <div className="card">
-      //         <div className="card-body text-center">
-      //           <img alt='Imagem do aluno' src="imagens/morena.jpg" width="260" height="160"></img>
-      //           <p className="card-text">Proximo Morena</p>
-      //         </div>
-      //       </div>
-
-      //       <div className="card">
-      //         <div className="card-body text-center">
-      //           <img alt='Imagem do aluno' src="imagens/neila.jpg" width="260" height="160"></img>
-      //           <p className="card-text">Proximo Neila</p>
-      //         </div>
-      //       </div>
-
-      //       <div className="card">
-      //         <div className="card-body text-center">
-      //           <img alt='Imagem do aluno' src="imagens/fabrine.jpg" width="260" height="160"></img>
-      //           <p className="card-text">Proximo Fabrine</p>
-      //         </div>
-      //       </div>
-      //     </div>
-
-
-
-      //     <div className="card-group">
-      //       <div className="card">
-      //         <div className="card-body text-center">
-      //           <img alt='Imagem do aluno' src="imagens/ronaldo.jpg" width="260" height="160"></img>
-      //           <p className="card-text">Proximo Ronaldo </p>
-      //         </div>
-      //       </div>
-      //       <div className="card">
-      //         <div className="card-body text-center">
-      //           <img alt='Imagem do aluno' src="imagens/giovana.jpg" width="260" height="160"></img>
-      //           <p className="card-text">Proximo Giovana</p>
-      //         </div>
-      //       </div>
-      //       <div className="card">
-      //         <div className="card-body text-center">
-      //           <img alt='Imagem do aluno' src="imagens/rose.jpg" width="260" height="160"></img>
-      //           <p className="card-text">Proximo Rosangela</p>
-      //         </div>
-      //       </div>
-      //       <div className="card">
-      //         <div className="card-body text-center">
-      //           <img alt='Imagem do aluno' src="imagens/lucas.jpg" width="260" height="160"></img>
-      //           <p className="card-text">Proximo Maicon</p>
-      //         </div>
-      //       </div>
-      //     </div>
-
-
-      //   </div>
-      // </div>
